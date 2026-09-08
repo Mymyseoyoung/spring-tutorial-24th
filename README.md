9bd11b9160e9dcd90fc20a37d70a3b78d67d8c77
박서영 <emilypark0721@gmail.com>
2026-09-08 19:42:53 +0900

    feat: spring-tutorial-24rd 완료

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# spring-tutorial-24th
-CEOS 백엔드 24기 스프링 튜토리얼
+### 1. spring-tutorial-24th
+
---
+++ src/main/java/com/ceos24/spring_boot/TestRepository.java
@@ -0,0 +1,7 @@
+package com.ceos24.spring_boot;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestRepository extends JpaRepository<Test, Long> {
+
+}
